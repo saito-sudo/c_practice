93e40d222fbf35208c650360cb22cf6d2f412c90
DESKTOP-TS8OS5I\UCRE <saito@ace-software.com>
2026-04-09 15:25:41 +0900

    演習6を登録。

---
+++ ensyuu06/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(void)
+{
+    int a=1;
+    int b=1;
+    int c;
+
+    printf("1～1000までのフィボナッチ数列を表示します。\n");
+    printf("%d,%d",a,b);
+
+    for(c=a+b;c<1000;c=a+b)
+    {
+        printf(",%d", c);
+        
+        a=b;
+        b=c;
+    }
+    printf("\n1000を超えたので、終了とします。");
+
+    return 0;
+}
