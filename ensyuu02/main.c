cde3825f0b1e8634a4ea882d74b00b6825163c1a
DESKTOP-TS8OS5I\UCRE <saito@ace-software.com>
2026-04-09 15:44:40 +0900

    演習2の判別のif文の書き方をif elseの形に変更し、i<aを√aまでをチェックする形に変更し、どの処理がどの判別かのコメントを入れました。

--- ensyuu02/main.c
+++ ensyuu02/main.c
@@ -7,10 +7,18 @@ int main(void)
     printf("整数を入力してください: ");
     scanf("%d", &a);
 
-    if (a == 0) printf("数はZEROです\n");
-    if (a > 0) printf("数は正の整数です\n");
-    if (a < 0) printf("数は負の整数です\n");
-
+    printf("判別1を以下に示す。");
+    if (a == 0){
+         printf("数はZEROです\n");
+    }else{
+        if (a > 0){
+             printf("数は正の整数です\n");
+        }else
+            if (a < 0){
+                printf("数は負の整数です\n");
+            }
+}
+    printf("判別2を以下に示す。");
     if (a == 0) {
         printf("----------\n");
     } else {
@@ -21,11 +29,12 @@ int main(void)
         }
     }
 
+    printf("判別3を以下に示す。");
     int i, is_prime = 1;
     if (a < 2) {
         is_prime = 0;
     } else {
-        for (i = 2; i < a; i++) {
+        for (i = 2; i*i<=a ; i++) {
             if (a % i == 0) {
                 is_prime = 0;
                 break;
